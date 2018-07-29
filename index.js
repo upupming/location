26024846c1f7ddb03efc2203d6e69d249e6791f2
upupming <upupming@gmail.com>
2018-07-29 11:46:46 +0800

    Added: Tecent map API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let x = document.getElementById("result");
 
 function showPosition(position) {
   console.log(position);
-  x.innerHTML = "纬度：" + position.coords.latitude + 
+  x.innerHTML = x.innerHTML +  '<hr> 浏览器 Geo API <hr>' + "纬度：" + position.coords.latitude + 
     "<br><br>经度：" + position.coords.longitude + 
     "<br><br>精度：" + position.coords.accuracy + "米";
 }
@@ -13,4 +13,19 @@ function getLocation() {
   } else  {
     x.innerHTML = "此浏览器不支持获取位置信息"
   }
+
+  let geo =  new qq.maps.Geolocation('FKLBZ-OH66W-N2ER5-RMVPT-4HINT-VWBDV', 'getLatLng');
+  
+  console.log(geo);
+
+  geo.getLocation(
+    showTecentPosition
+  );
+}
+
+function showTecentPosition(position) {
+  console.log(position);
+  x.innerHTML = x.innerHTML + '<hr> 腾讯地图 API <hr>' + "纬度：" + position.lat + 
+    "<br><br>经度：" + position.lng + 
+    "<br><br>精度：" + position.accuracy + "米";
 }
